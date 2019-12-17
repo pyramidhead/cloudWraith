d3cf066293dbaa0c92737aa98b065b1254bfc394
pyramidhead <jasondanielwall@gmail.com>
2019-12-17 10:38:47 -0800

    automation/integration testing

--- envPrep.sh
+++ envPrep.sh
@@ -3,7 +3,7 @@
 
 # install linux dependencies
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -f \
 	apt-transport-https \
 	ca-certificates \
 	curl \
@@ -17,11 +17,14 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -f docker-ce docker-ce-cli containerd.io
+echo Initial health check:
 sudo docker run hello-world
+echo 
 sudo usermod -aG docker $USER
 
 # validate docker from another shell
 spineCheck="$(~/cloudWraith/docker/dockerCheck.sh)";
 echo Shelled health check:
 echo $spineCheck
+echo 
