2c89d59ba06caf1d16850a3f77fdbe9d624589e3
pyramidhead <jasondanielwall@gmail.com>
2019-12-17 10:16:33 -0800

    dependency cleanup and bugfix on docker validation

--- envPrep.sh
+++ envPrep.sh
@@ -8,9 +8,7 @@ sudo apt-get install \
 	ca-certificates \
 	curl \
 	software-properties-common \
-	gnupg-agent \
-	mysql-client-core-5.7 \
-	mariadb-client-10.1
+	gnupg-agent
 
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -21,14 +19,9 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo docker run hello-world
-sudo groupadd docker
 sudo usermod -aG docker $USER
-# validate group membership
-# docker run hello-world (we have to do this in a new shell so group membership applies)
-spineCheck="$(~/docker/dockerCheck.sh)";
+
+# validate docker from another shell
+spineCheck="$(~/cloudWraith/docker/dockerCheck.sh)";
 echo Shelled health check:
 echo $spineCheck
-
-# initial git pull
-
-# add call to make pull.sh executable if possible
