ed1c221fd371a788fc736a131ea620d88f3c5fbb
pyramidhead <jasondanielwall@gmail.com>
2019-12-17 09:53:11 -0800

    integration test for environment build

--- envPrep.sh
+++ envPrep.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
 # dev environment preparer to work with cloudWraith
+
+# install linux dependencies
 sudo apt-get update
 sudo apt-get install \
 	apt-transport-https \
@@ -8,6 +11,8 @@ sudo apt-get install \
 	gnupg-agent \
 	mysql-client-core-5.7 \
 	mariadb-client-10.1
+
+# install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -18,5 +23,12 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo docker run hello-world
 sudo groupadd docker
 sudo usermod -aG docker $USER
-# validate group membership here
-docker run hello-world
+# validate group membership
+# docker run hello-world (we have to do this in a new shell so group membership applies)
+spineCheck="$(~/docker/dockerCheck.sh)";
+echo Shelled health check:
+echo $spineCheck
+
+# initial git pull
+
+# add call to make pull.sh executable if possible
