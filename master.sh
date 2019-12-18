b091bc35e892952f63ad8837381e56c038f922fe
pyramidhead <jasondanielwall@gmail.com>
2019-12-18 07:35:08 -0800

    being paranoid about this check

--- master.sh
+++ master.sh
@@ -8,8 +8,9 @@ docker run -d --rm --name satchel -d mongo:4.0.4;
 docker ps -a;
 
 # validate mongo service availability
-sleep 5
 mongoHealth="$(docker exec satchel mongo --eval "printjson(db.serverStatus())" | grep "Implicit session")"
 while [ -z "$mongoHealth" ]; do
-	sleep 5
+	sleep 1
+	echo "Mongo not awake yet."
+	mongoHealth="$(docker exec satchel mongo --eval "printjson(db.serverStatus())" | grep "Implicit session")"
 done
