337b99fe65e4e57d96c887058308c4536d442b31
pyramidhead <jasondanielwall@gmail.com>
2019-12-18 06:00:15 -0800

    this works, stripping debug echos

--- envPrep.sh
+++ envPrep.sh
@@ -10,23 +10,18 @@ sudo apt-get -f install mongodb-clients
 
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-echo Adding docker repository
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-echo Running apt-get update
 sudo apt-get update
-echo Installing known dependencies
 sudo apt-get -f install aufs-tools
 sudo apt-get -f install cgroupfs-mount
 sudo apt-get -f install libltdl7
 sudo apt-get -f install pigz
-echo Running docker package installs
 sudo apt-get -f install docker-ce-cli
 sudo apt-get -f install containerd.io
 sudo apt-get -f install docker-ce
-echo Initial health check:
 sudo docker run hello-world
 
 # final linux side steps; group membership and file permissions
