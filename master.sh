242e3d674466f427e643b87d072d42621b346b9b
pyramidhead <jasondanielwall@gmail.com>
2019-12-18 05:56:44 -0800

    troubleshooting mongo port availability

--- master.sh
+++ master.sh
@@ -2,17 +2,18 @@
 # master script to operate a containerized pentesting architecture organized around dorking threat vectors identified with metasploit
 # prerequisites: docker
 
-# start mongo container and validate
+# start mongo container and validate in docker
 docker pull mongo:4.0.4;
 docker run -d --rm --name satchel -d mongo:4.0.4;
 docker ps -a;
 
-# modify this for mongo
-# until nc -z -v -w30 $satchelHost 27017; do
-#	sleep 5;
-#	echo "poking mongo. he's growling."
-#	mongoExternalQuery="$(mysql -uroot -pSQUIRREL -e 'use performance_schema')"
-# done
-
-# validate mongo container availability
-# ./justPawn.sh
+# validate mongo service availability
+until nc -z -v -w30 $satchelHost 27017; do
+	sleep 5;
+	echo "Poking mongo. He's growling."
+	# define query from successful health check
+	mongoHealth="$(mongo --eval "printjson(db.serverStatus())")"
+	# another until loop for success case
+		# sleep 5;
+		# echo "Gave mongo coffee. Give him a moment."
+done;
