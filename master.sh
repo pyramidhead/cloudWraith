ec4aa785c13a7cc225ba8097f397408750036a07
pyramidhead <jasondanielwall@gmail.com>
2019-12-18 07:47:57 -0800

    more health check improvements

--- master.sh
+++ master.sh
@@ -8,9 +8,10 @@ docker run -d --rm --name satchel -d mongo:4.0.4;
 docker ps -a;
 
 # validate mongo service availability
-mongoHealth="$(docker exec satchel mongo --eval "printjson(db.serverStatus())" | grep "Implicit session")"
-while [ -z "$mongoHealth" ]; do
-	sleep 1
+mongoHealth="$(docker exec satchel mongo --eval "printjson(db.serverStatus())" | grep "Implicit")"
+until [[ $mongoHealth =~ "Implicit" ]]; do
 	echo "Mongo not awake yet."
-	mongoHealth="$(docker exec satchel mongo --eval "printjson(db.serverStatus())" | grep "Implicit session")"
+	sleep 1
+	mongoHealth="$(docker exec satchel mongo --eval "printjson(db.serverStatus())" | grep "Implicit")"
 done
+echo "Mongo awake."
