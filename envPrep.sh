2595aa4518b3a27432523e045bd9cbb7c60a392c
pyramidhead <jasondanielwall@gmail.com>
2019-12-17 20:16:57 -0800

    adding echo for troubleshooting

--- envPrep.sh
+++ envPrep.sh
@@ -9,11 +9,14 @@ sudo apt-get -f install curl
 
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo Adding docker repository.
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+echo Running apt-get update.
 sudo apt-get update
+echo Running docker package installs.
 sudo apt-get -f install docker-ce
 sudo apt-get -f install docker-ce-cli
 sudo apt-get -f install containerd.io
