82dfb888f3f0ce07405e73c474d0da40aa189ac6
pyramidhead <jasondanielwall@gmail.com>
2019-12-18 06:52:42 -0800

    trying simpler mongo health check

--- master.sh
+++ master.sh
@@ -8,11 +8,13 @@ docker run -d --rm --name satchel -d mongo:4.0.4;
 docker ps -a;
 
 # validate mongo service availability
-until nc -z -v -w30 $satchelHost 27017; do
-	sleep 5;
-	echo "Poking mongo. He's growling."
+mongoHealth="$(docker exec satchel mongo --eval "printjson(db.serverStatus())" | grep "Implicit session")"
+echo $mongoHealth
+# until nc -z -v -w30 $satchelHost 27017; do
+#	sleep 5;
+#	echo "Poking mongo. He's growling."
 	# define query from successful health check
-	mongoHealth="$(mongo --eval "printjson(db.serverStatus())")"
+	# mongoHealth="$(docker exec satchel mongo --eval "printjson(db.serverStatus())")"
 	# another until loop for success case
 		# sleep 5;
 		# echo "Gave mongo coffee. Give him a moment."
