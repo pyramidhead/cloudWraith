f7b4355e5bf338b943e63ee2b24c6b4dc13c5381
pyramidhead <jasondanielwall@gmail.com>
2019-12-18 22:10:34 -0800

    working on grub next

--- agent.sh
+++ agent.sh
@@ -25,7 +25,8 @@ echo $rubyHealth
 if [[ $rubyHealth =~ "file not found" ]]; then
 	# ruby install
 	docker exec scalpel apt-get update
-	docker exec scalpel apt-get -fy install grub
+	docker exec scalpel mkdir ~/grub
+	docker exec scalpel grub-install ~/grub
 	docker exec scalpel apt-get -fy install snapd
 	docker exec scalpel PATH=$PATH:/snap/bin
 	docker exec scalpel snap install ruby --classic
