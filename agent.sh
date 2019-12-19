5c8002f88f4c335cfb6349bdf734e3367043ef2f
pyramidhead <jasondanielwall@gmail.com>
2019-12-18 21:17:50 -0800

    forgot to do apt-get update first

--- agent.sh
+++ agent.sh
@@ -24,8 +24,8 @@ rubyHealth="$(docker exec scalpel ruby -v | grep "file not found")"
 echo $rubyHealth
 if [[ $rubyHealth =~ "file not found" ]]; then
 	# ruby install
-	docker exec scalpel apt-get -fy install software-properties-common
 	docker exec scalpel apt-get update
+	docker exec scalpel apt-get -fy install software-properties-common
 	docker exec scalpel apt-add-repository ppa:brightbox/ruby-ng
 	docker exec scalpel apt-get -fy install ruby2.3 ruby2.3-dev
 	docker ps -a
