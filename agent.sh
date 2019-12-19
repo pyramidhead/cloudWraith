8517c8ebebc5e971eb03e1813bb13e026f183def
pyramidhead <jasondanielwall@gmail.com>
2019-12-19 11:25:31 -0800

    more stackoverflow copypasta

--- agent.sh
+++ agent.sh
@@ -31,6 +31,7 @@ if [[ $rubyHealth =~ "file not found" ]]; then
 	# sometimes this call fails and stalls, may need handling for that
 	docker exec scalpel gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 	# what we need here is to write trust for the imported keys
+	echo "$( gpg --list-keys --fingerprint | grep "409B6B1796C275462A1703113804BB82D39DC0E3" -B 1 | head -1 | tr -d '[:space:]'|awk 'BEGIN { FS = "=" } ; { print $2 }' ):6:" | gpg --import-ownertrust;
 	docker exec scalpel curl -L get.rvm.io | bash -s stable
 fi
 # ruby validate
