c33deb701ca97ad71b133c91ecd39a4558a377d8
pyramidhead <jasondanielwall@gmail.com>
2019-12-25 10:18:13 -0800

    cleanup and regression. next task: begin metasploit install in dockerfile

--- agent.sh
+++ agent.sh
@@ -19,19 +19,12 @@ until [[ $mongoHealth =~ "Implicit" ]]; do
 	mongoHealth="$(docker exec legdrop mongo --eval "printjson(db.serverStatus())" | grep "Implicit")"
 done
 
-# start kali VM, validate in docker, and spawn shell session
+# start kali VM and validate in docker
 docker image build -t drawer ./kali
 docker inspect drawer
 docker run -t -d --rm --mount source=backpack,target=/usr/local/cloudWraith --name scalpel drawer
 docker ps -a
 
-# validate postgresql service from dockerfile
-
 # validate metasploit health
-metasploitHealth="$(docker exec scalpel msfupdate)"
-
-# metasploitHealth="$(docker exec scalpel msf > help | grep "Description")"
-# echo $metasploitHealth
-
-# fetch gpg2 pubkey
-# gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+metasploitHealth="$(docker exec scalpel msf > help | grep "Description")"
+# metasploitHealth="$(docker exec scalpel msfupdate)"
