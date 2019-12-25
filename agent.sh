bd9793ab6c45568b84ef82893812a82238c7e691
pyramidhead <jasondanielwall@gmail.com>
2019-12-25 12:19:42 -0800

    frameworking metasploit dockerfile

--- agent.sh
+++ agent.sh
@@ -17,7 +17,7 @@ until [[ $mongoHealth =~ "Implicit" ]]; do
 	mongoHealth="$(docker exec legdrop mongo --eval "printjson(db.serverStatus())" | grep "Implicit")"
 done
 
-# build kali container
+# build and validate kali container
 docker image build -t drawer ./kali
 docker inspect drawer
 docker run -t -d --rm --mount source=backpack,target=/usr/local/cloudWraith --name maglite drawer
@@ -25,11 +25,12 @@ docker ps -a
 kaliHealth="$(docker exec maglite cat /etc/os-release | grep "ID_LIKE")"
 until [[ $kaliHealth =~ "ID_LIKE" ]]; do
 	sleep 1
-	echo "Testing kali version."
 	kaliHealth="$(docker exec maglite cat /etc/os-release | grep "ID_LIKE")"
 done
 
 # build remnux metasploit container
+docker image build -t drawer ./metasploit
+docker inspect drawer
 docker run -t -d --rm -p 443:443 --mount source=backpack,target=/usr/local/cloudWraith --name scalpel remnux/metasploit
 docker ps -a
 metasploitHealth="$(docker exec scalpel msf > help | grep "Description")"
