ee40e22edd470074415a74241cd7d578084332ed
pyramidhead <jasondanielwall@gmail.com>
2019-12-31 07:50:59 -0800

    mount reconfig and validation

--- agent.sh
+++ agent.sh
@@ -8,7 +8,7 @@ docker volume inspect backpack
 
 # build and validate mongo container
 docker image build -t drawer ./mongo
-docker run -d --rm --mount source=backpack,target=/cloudWraith --name legdrop drawer
+docker run -d --rm --mount source=backpack,target=/backpack --name legdrop drawer
 mongoHealth="$(docker exec legdrop mongo --eval "printjson(db.serverStatus())" | grep "Implicit")"
 until [[ $mongoHealth =~ "Implicit" ]]; do
 	sleep 1
@@ -17,7 +17,7 @@ done
 
 # build and validate kali container
 docker image build -t drawer ./kali
-docker run -t -d --rm --mount source=backpack,target=/cloudWraith --name scalpel drawer
+docker run -t -d --rm --mount source=backpack,target=/backpack --name scalpel drawer
 kaliHealth="$(docker exec scalpel cat /etc/os-release | grep "ID_LIKE")"
 until [[ $kaliHealth =~ "ID_LIKE" ]]; do
 	sleep 3
