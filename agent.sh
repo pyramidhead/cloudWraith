340462cb3f1a9c1dea3643fe9cae444570643f27
pyramidhead <jasondanielwall@gmail.com>
2019-12-31 06:04:04 -0800

    may have to move some commands back to shell

--- agent.sh
+++ agent.sh
@@ -24,6 +24,7 @@ until [[ $kaliHealth =~ "ID_LIKE" ]]; do
 	kaliHealth="$(docker exec scalpel cat /etc/os-release | grep "ID_LIKE")"
 done
 echo "goddess Kali has awoken"
+# may need to move some of metasploit build back to shell, it's not working in dockerfile
 # health check metasploit app
 metasploitAppHealth="$(docker exec scalpel msfconsole | grep "encoders")"
 until [[ $metasploitAppHealth =~ "encoders" ]]; do
