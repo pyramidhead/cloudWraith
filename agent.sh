bb2f704b9aa4f70e70c4e26df2f69b278930fd0a
pyramidhead <jasondanielwall@gmail.com>
2019-12-31 05:53:56 -0800

    removing mount command, scalpel does not need direct file access

--- agent.sh
+++ agent.sh
@@ -17,7 +17,7 @@ done
 
 # build and validate kali container
 docker image build -t drawer ./kali
-docker run -t -d --rm --mount source=backpack,target=/usr/local/cloudWraith --name scalpel drawer
+docker run -t -d --rm --name scalpel drawer
 kaliHealth="$(docker exec scalpel cat /etc/os-release | grep "ID_LIKE")"
 until [[ $kaliHealth =~ "ID_LIKE" ]]; do
 	sleep 3
